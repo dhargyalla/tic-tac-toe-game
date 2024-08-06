6b1062296bd657eb982a272ae8aeb4c6db9716d1
Tsering Dhargyal <dhargyalofficial@gmail.com>
2024-08-06 11:23:15 +0100

    add about.html

--- main.py
+++ main.py
@@ -2,13 +2,9 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def hello_world():
-#     return 'Holla tsering'
-
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run()
