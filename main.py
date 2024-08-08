0175f1035e190d3c0b5e1b40ac7acd6eb436ce51
Tsering Dhargyal <dhargyalofficial@gmail.com>
2024-08-08 15:41:26 +0100

    update files

--- main.py
+++ main.py
@@ -12,13 +12,4 @@
 
 # main.py
 
-from morse_code import text_to_morse
-
-def main():
-    # Example: Getting user input and converting it to Morse code
-    user_input = input("Enter text to convert to Morse code: ")
-    morse_output = text_to_morse(user_input)
-    print(f"Morse Code: {morse_output}")
-
-if __name__ == "__main__":
-    main()
+print('hello world')
