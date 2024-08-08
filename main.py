735c4d0d558c9b5542bf58a32c41c915822c9b0f
Tsering Dhargyal <dhargyalofficial@gmail.com>
2024-08-08 15:38:38 +0100

    add some files

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from flask import Flask,render_template
+# from flask import Flask,render_template
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def home():
+#     return 'this is a good day'
+#
+# if __name__ == '__main__':
+#     app.run()
 
-app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'this is a good day'
+# main.py
 
-if __name__ == '__main__':
-    app.run()
+from morse_code import text_to_morse
+
+def main():
+    # Example: Getting user input and converting it to Morse code
+    user_input = input("Enter text to convert to Morse code: ")
+    morse_output = text_to_morse(user_input)
+    print(f"Morse Code: {morse_output}")
+
+if __name__ == "__main__":
+    main()
---
+++ morse_code.py
@@ -0,0 +1,25 @@
+# morse_code.py
+
+MORSE_CODE_DICT = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
+    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--',
+    'Z': '--..',
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+    '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----',
+    ', ': '--..--', '.': '.-.-.-', '?': '..--..', '/': '-..-.', '-': '-....-',
+    '(': '-.--.', ')': '-.--.-'
+}
+
+def text_to_morse(text):
+    morse_code = []
+    for char in text.upper():
+        if char in MORSE_CODE_DICT:
+            morse_code.append(MORSE_CODE_DICT[char])
+        elif char == ' ':
+            morse_code.append(' / ')  # Use ' / ' to represent space between words
+        else:
+            morse_code.append('?')  # Unknown characters are represented by '?'
+    return ' '.join(morse_code)
